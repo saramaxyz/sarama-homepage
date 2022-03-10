ac413eaa29e5fbffd6d1ddb7d5a98eb84a4ddb2b
Praful Mathur <praful.mathur@gmail.com>
2022-03-10 14:09:03 -0800

    Added assets and new header for new fonts

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+	  <style>
+          @import url('https://fonts.googleapis.com/css2?family=Jost:wght@600&display=swap');
+          </style>
+	</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
